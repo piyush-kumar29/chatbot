31b3ebfae94e531be69c340e0dd817decf124a8d
piyush-kumar29 <pt3943945@gmail.com>
2026-04-25 19:09:30 +0530

    fixed api url

--- client/src/components/Chatbot.jsx
+++ client/src/components/Chatbot.jsx
@@ -40,7 +40,7 @@ const Chatbot = ({ isOpen, onClose }) => {
         setIsLoading(true);
 
         try {
-            const res = await axios.post('http://localhost:5000/api/chat', {
+            const res = await axios.post('https://chatbot-og7m.onrender.com/api/chat', {
                 sessionId: sid,
                 message: msgText
             });
